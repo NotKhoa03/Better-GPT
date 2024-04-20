ccc8970d37ae3ca2b173c2a34f8d43cb40c0607b
Khoa Tonthat <henry.tonthat.2014@gmail>
2024-04-20 00:35:08 -0500

    Fixed deployment issue

--- back-end/venv/main.py
+++ back-end/venv/main.py
@@ -1,15 +1,23 @@
-from flask import Flask, jsonify, request
+from flask import Flask, send_from_directory
+import os
 from flask_cors import CORS
 import pdf
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../../front-end/dist')  # Assuming your React app is built into a 'dist' directory
 cors = CORS(app, origins='*')
 
 @app.route("/PDF", methods=['POST'])
 def pdf_summary():
     return pdf.pdf_summary()
-   
-# @app.route("/youtube", methods=['POST'])
-# def 
+
+# Serve the React app
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
